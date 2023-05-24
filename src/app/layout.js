c6926fd3ccffc9379d71a59fd8a9d11570230b08
PeteNelson22 <peternelson063@gmail.com>
2023-05-24 21:16:03 +0100

    Added Header component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,18 @@
-import './globals.css'
+import Header from "@/components/Header";
+import "./globals.css";
 
 export const metadata = {
-  title: 'IMDB',
-  description: 'IMDB Clone',
-}
+  title: "IMDB",
+  description: "IMDB Clone",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
