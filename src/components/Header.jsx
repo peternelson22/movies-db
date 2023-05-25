320cd7a85f5ea0da32ac0afb3df2808b3704a295
PeteNelson22 <peternelson063@gmail.com>
2023-05-25 10:09:05 +0100

    Added dark mode funtionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,26 @@
 import Link from "next/link";
 import MenuItem from "./MenuItem";
+import DarkMode from "./DarkMode";
 import { AiFillHome } from "react-icons/ai";
 import { BsFillInfoCircleFill } from "react-icons/bs";
 
 const Header = () => {
   return (
-    <div className="flex justify-between mx-2 max-w-6xl sm:mx-auto items-center py-6">
-      <div className="flex">
+    <div className="flex justify-between mx-6 max-w-6xl sm:mx-auto items-center py-6">
+      <div className="flex font-mono font-semibold">
         <MenuItem title="Home" address="/" Icon={AiFillHome} />
         <MenuItem title="About" address="/about" Icon={BsFillInfoCircleFill} />
       </div>
-      <div className="flex">
+      <div className="flex items-center space-x-5">
+        <DarkMode />
         <Link href="/">
           <h2 className="text-2xl">
-            <span className="font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1">
+            <span className="font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1 font-mono">
               IMDb
             </span>
-            <span className="text-xl hidden sm:inline">Clone</span>
+            <span className="text-xl hidden sm:inline sm:mr-4 lowercase font-mono">
+              Clone
+            </span>
           </h2>
         </Link>
       </div>
