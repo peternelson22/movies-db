88acc4e567983b494ed3989141dd42565f5a0eda
PeteNelson22 <peternelson063@gmail.com>
2023-05-25 18:22:22 +0100

    Added about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,39 @@
+const About = () => {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600 ">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut odio,
+        libero alias, similique facere vero incidunt non sit vitae excepturi,
+        saepe dicta. Cupiditate veritatis obcaecati voluptate fugiat ab sequi,
+        eum dignissimos officiis officia ipsa sed odio enim commodi tempora!
+        Nihil possimus vitae architecto totam incidunt fugiat esse fuga magni.
+        Possimus distinctio earum ad amet unde ipsam perferendis ullam eum id
+        aliquam. Eum ea dignissimos dolore. Quidem omnis culpa assumenda
+        veritatis blanditiis exercitationem dolores dolorem cumque temporibus
+        laboriosam fugiat perferendis pariatur ipsam libero, earum maxime
+        quisquam obcaecati voluptates ex. Nemo sed commodi voluptate laboriosam
+        officiis doloribus totam maxime quasi autem est?
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae a
+        molestiae, sed recusandae quos possimus excepturi! Id fugiat eligendi
+        voluptas inventore, provident tempore debitis similique! Rem, minus
+        eligendi dolorum quod fugiat, voluptates qui quo praesentium
+        perspiciatis tenetur earum totam labore.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint quas
+        tempore molestias maiores. Aut provident amet natus quidem recusandae
+        ipsum porro repellat et rem quis ab cumque, maxime hic odit blanditiis
+        dignissimos delectus fuga quod velit architecto sunt quam? Eos omnis
+        natus debitis eligendi ex quidem error ducimus maiores aliquam rem
+        deleniti obcaecati officiis asperiores perferendis perspiciatis ad a
+        quisquam necessitatibus sit, saepe eveniet culpa doloribus quia! Nam
+        molestias debitis molestiae alias consectetur officiis! Eius,
+        consectetur in?
+      </p>
+    </div>
+  );
+};
+export default About;
