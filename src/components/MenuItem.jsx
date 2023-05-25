04c4a9dbe8d8d2f14d31b0d6f83dbc4f27365fe4
PeteNelson22 <peternelson063@gmail.com>
2023-05-26 00:03:20 +0100

    Fetch data from TMDB API

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,14 +1,15 @@
-import Link from "next/link";
+import Link from 'next/link'
 
 const MenuItem = ({ title, address, Icon }) => {
   return (
     <div>
-      
-      <Link href={address} className="mx-4 lg:mx-6 hover:text-amber-600">
-        <Icon className="text-2xl sm:hidden mx-4" />
-        <p className="hidden sm:inline my-2 text-sm uppercase tracking-wider">{title} </p>
+      <Link href={address} className='mx-4 lg:mx-6 hover:text-amber-600'>
+        <Icon className='text-2xl sm:hidden mx-4' />
+        <p className='hidden sm:inline my-2 text-sm uppercase tracking-wider'>
+          {title}{' '}
+        </p>
       </Link>
     </div>
-  );
-};
-export default MenuItem;
+  )
+}
+export default MenuItem
